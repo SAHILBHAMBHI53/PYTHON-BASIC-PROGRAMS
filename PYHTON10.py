8757f17f32b876ff97e1100dbd0e0f6a891ae258
SAHIL BHAMBHI <sahilbhambhi@gmail.com>
2026-07-29 22:24:37 +0530

    Average for three numbers
    Fixed average calculation and added input prompts.

---
+++ PYHTON10.py
@@ -0,0 +1,9 @@
+#Average Of the Number
+
+Num1 = int(input("Enter number:"))
+Num2 = int(input("Enter number:"))
+Num3 = int(input("Enter number:"))
+
+Avarege = (Num1 + Num2 + Num3 / 3)
+
+print("Average",Avarege)
