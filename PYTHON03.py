699fafdfd521113f75903776a6dc047542f02ec0
SAHIL BHAMBHI <sahilbhambhi@gmail.com>
2026-07-29 18:55:28 +0530

    Multiplication program for two numbers

---
+++ PYTHON03.py
@@ -0,0 +1,8 @@
+# Multiplication of Two Numbers
+
+Num1 = int(input("Enter Your Number:"))
+Num2 = int(input("Enter Your Number:"))
+
+Multiplication = (Num1 * Num2)
+
+print("Multiplication =", Multiplication) 
