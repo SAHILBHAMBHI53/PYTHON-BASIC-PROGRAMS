15f8dff2f3bd3a858c4b2e97fbc9e4485b344be2
SAHIL BHAMBHI <sahilbhambhi@gmail.com>
2026-07-29 18:45:03 +0530

    Subtraction of Two Numbers
    Subtraction of Two Numbers

---
+++ PYTHON02.py
@@ -0,0 +1,7 @@
+# Subtraction of Two Numbers
+
+Num1 = int(input("Enter Your Number:"))    #user input
+Num2 = int(input("Enter Your Number:"))    #user input
+
+Subtraction = Num1 - Num2
+print("Subtraction =", Subtraction)
