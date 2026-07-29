3cb51e55437d27f44cfc00e6a68beb8eb990ebca
SAHIL BHAMBHI <sahilbhambhi@gmail.com>
2026-07-29 18:37:28 +0530

    Add script for addition of two numbers

---
+++ PYTHON01.py
@@ -0,0 +1,8 @@
+#Addithion of  Two Numbers.
+
+Num1 = int(input("Enter Your Number1 for Addition:"))
+Num2 = int(input("Enter Your Number2 for Addition:"))
+
+Total = Num1 + Num2
+
+print("Sum=:",Total)
