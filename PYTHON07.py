c3ca892dd1fde4004ebe9f19a807df15db24e223
SAHIL BHAMBHI <sahilbhambhi@gmail.com>
2026-07-29 21:43:04 +0530

    Calculate area of a rectangle

---
+++ PYTHON07.py
@@ -0,0 +1,8 @@
+# Area of a Rectangle
+
+Height = int(input("Enter Rectangle Hight:"))
+Length = int(input("Enter Rectangle Lenght:"))
+
+Area = (Height * Length)
+
+print("Area Of Rectangle:",Area)
